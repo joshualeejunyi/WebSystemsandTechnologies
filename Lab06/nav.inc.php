93d9d54ba6a176152a76160ff6387c17b05a5d1d
Joshua Lee <joshua.lee.99@hotmail.com>
2020-02-15 22:42:39 +0800

    lab 6

---
+++ Lab06/nav.inc.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><img src="images/logo.png" height="50"></a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobilenav" aria-controls="mobilenav" aria-expanded="false" aria-label="Mobile Nav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="mobilenav">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php#dogs">Dogs</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php#cats">Cats</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="register.php">Register</a>
+            </li>
+        </ul>
+    </div>
+
+</nav>
